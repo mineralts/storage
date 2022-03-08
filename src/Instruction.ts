6a60d3c10e277c74d325b1ed9c2e3a33eb4881bd
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-08 14:22:40 +0100

    :construction: Work in progress

---
+++ src/Instruction.ts
@@ -0,0 +1,39 @@
+import { MineralPlugin } from '@mineralts/core-preview'
+import { join } from 'node:path'
+import fs from 'node:fs/promises'
+
+export default class Instruction extends MineralPlugin {
+  public async configure (): Promise<void> {
+    const environment = this.ioc.resolveBinding('Mineral/Core/Environment')
+    const root = environment.resolveKey('root')
+
+    await this.alterRcFile(root)
+    await this.writeConfig(root)
+  }
+
+  protected async alterRcFile (root) {
+    const moduleName: string = '@mineralts/storage-preview'
+    const rcFileLocation = join(root, '.mineralrc.json')
+
+    const { default: rcFile } = await import(rcFileLocation)
+    if (!rcFile.providers) {
+      rcFile.providers = []
+    }
+
+    if (!rcFile.providers.includes(moduleName)) {
+      rcFile.providers.push(moduleName)
+
+      await fs.writeFile(
+        rcFileLocation,
+        JSON.stringify(rcFile, null, 2)
+      )
+    }
+  }
+
+  protected async writeConfig (root) {
+    const configLocation = join(root, 'config', 'database.ts')
+    const template = await fs.readFile(join(__dirname, '..', 'templates', 'database.txt'))
+
+    await fs.writeFile(configLocation, template)
+  }
+}
