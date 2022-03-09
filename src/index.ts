123e9196abfb25b0b5a6687fe0c988ca611bd73f
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-09 16:06:17 +0100

    :construction: Work in progress

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,12 @@
  * file that was distributed with this source code.
  *
  */
+import { Model as MineralModel, NonAttribute as NotColumn } from 'sequelize'
 
 export * from './types'
+export * from './entities'
+export {
+  MineralModel,
+  NotColumn
+}
+export { Optional, DataTypes, CreationOptional, InferAttributes, InferCreationAttributes } from 'sequelize'
