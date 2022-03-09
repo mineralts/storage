07cc90db9656b5cf0bae47b0f215ad7763397dc8
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-09 16:41:23 +0100

    :construction: Work in progress

--- src/providers/StorageProvider.ts
+++ src/providers/StorageProvider.ts
@@ -8,11 +8,12 @@
  *
  */
 
-import { MineralProvider, Provider } from '@mineralts/core-preview'
+import { Entity, MineralProvider, Provider } from '@mineralts/core-preview'
 import Storage from '../Storage'
+import { Model, ModelStatic } from 'sequelize'
 
 @Provider()
-export default class AppProvider extends MineralProvider {
+export default class StorageProvider extends MineralProvider {
   public async boot () {
     const environment = this.application.resolveBinding('Mineral/Core/Environment')
     const debug = environment.resolveKey('debug')
@@ -27,7 +28,43 @@ export default class AppProvider extends MineralProvider {
     this.application.registerBinding('Mineral/Modules/Storage', storage)
   }
 
-  public async loadFile (file) {
+  public async loadFile (file: Entity) {
+    const helper = this.application.resolveBinding('Mineral/Core/Helpers')
+    const storage = this.application.resolveBinding('Mineral/Modules/Storage') as Storage
+
+    if (file.source.identifier == 'storage-model') {
+      const model = file.source
+      model.init(model.databaseSchema, {
+        sequelize: storage.sequelize,
+        modelName: helper.snakeCase(model.name),
+        timestamps: true,
+        createdAt: true,
+      })
+
+      Object.entries(model.relations).forEach(([, payload]: [string, any]) => {
+        const relatedModel = payload.relatedModel() as ModelStatic<Model>
+        const foreignKey = helper.camelCase(helper.singularize(`${model.name}Id`))
+        const pivotTableName = helper.snakeCase(helper.singularize(`${model.name}_${payload.relatedModel().name}`))
+
+        const relations = {
+          hasOne: () => { model.hasOne(relatedModel, { foreignKey })  },
+          hasMany: () => { model.hasMany(relatedModel, { foreignKey }) },
+          belongTo: () => { relatedModel.belongsTo(model) },
+          manyToMany: () => {
+            model.belongsToMany(payload.relatedModel(), {
+              through: pivotTableName,
+              foreignKey,
+              onDelete: payload.onDelete || 'SET NULL',
+              onUpdate: payload.onUpdate || 'SET NULL'
+            })
+          }
+        }
+
+        if (payload.relationType in relations) {
+          relations[payload.relationType]()
+        }
+      })
+    }
   }
 
   public async ok () {
